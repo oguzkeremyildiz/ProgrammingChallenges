d24d8553a683eca54d920a62a0c7bea6556859da
oguzkeremyildiz <oguzkeremyildiz@gmail.com>
2020-01-17 23:19:56 +0300

    Initial Commit

---
+++ src/DataStructures/SuitComparator.java
@@ -0,0 +1,19 @@
+package DataStructures;/* Created by oguzkeremyildiz on 17.01.2020 */
+
+import java.util.Comparator;
+
+public class SuitComparator implements Comparator<Card> {
+
+    @Override
+    public int compare(Card o1, Card o2) {
+        if (o1.getSuit().compareTo(o2.getSuit()) < 0){
+            return -1;
+        } else {
+            if (o1.getSuit().compareTo(o2.getSuit()) > 0){
+                return 1;
+            } else {
+                return o1.getValue() - o2.getValue();
+            }
+        }
+    }
+}
