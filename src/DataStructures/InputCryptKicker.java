95cc5567a5d97d3e71662d7280bdb3518225b165
oguzkeremyildiz <oguzkeremyildiz@gmail.com>
2020-01-20 20:50:43 +0300

    Initial Commit

---
+++ src/DataStructures/InputCryptKicker.java
@@ -0,0 +1,31 @@
+package DataStructures;/* Created by oguzkeremyildiz on 19.01.2020 */
+
+import java.io.BufferedWriter;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+
+public class InputCryptKicker {
+    public static void saveAsXml(String fileName) {
+        BufferedWriter outfile;
+        try {
+            OutputStreamWriter writer = new OutputStreamWriter(new FileOutputStream(fileName), "UTF-8");
+            outfile = new BufferedWriter(writer);
+            outfile.write("6 \n");
+            outfile.write("and \n");
+            outfile.write("dick \n");
+            outfile.write("jane \n");
+            outfile.write("puff \n");
+            outfile.write("spot \n");
+            outfile.write("yertle \n");
+            outfile.write("bjvg xsb hxsn xsb qymm xsb rqat xsb pnetfn \n");
+            outfile.write("xxxx yyy zzzz www yyyy aaa bbbb ccc dddddd \n");
+            outfile.close();
+        } catch (IOException ioException) {
+            System.out.println("Output file can not be opened");
+        }
+    }
+    public static void main(String[]args){
+        saveAsXml("Crypt.txt");
+    }
+}
