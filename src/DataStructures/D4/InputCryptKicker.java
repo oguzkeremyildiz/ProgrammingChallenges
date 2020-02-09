f785f71899912703f359672c7e8ecb51dcc6d857
oguzkeremyildiz <oguzkeremyildiz@gmail.com>
2020-02-09 12:00:23 +0300

    Initial Commit

--- src/DataStructures/D4/InputCryptKicker.java
+++ src/DataStructures/D4/InputCryptKicker.java
@@ -4,12 +4,13 @@ import java.io.BufferedWriter;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.OutputStreamWriter;
+import java.nio.charset.StandardCharsets;
 
 public class InputCryptKicker {
     public static void saveAsXml(String fileName) {
         BufferedWriter outfile;
         try {
-            OutputStreamWriter writer = new OutputStreamWriter(new FileOutputStream(fileName), "UTF-8");
+            OutputStreamWriter writer = new OutputStreamWriter(new FileOutputStream(fileName), StandardCharsets.UTF_8);
             outfile = new BufferedWriter(writer);
             outfile.write("6 \n");
             outfile.write("and \n");
