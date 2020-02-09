c806d1e0c7e86807705d88ccf089dffcde7ac684
oguzkeremyildiz <oguzkeremyildiz@gmail.com>
2020-02-09 11:58:13 +0300

    Initial Commit

--- src/DataStructures/D1/InputJollyJumpers.java
+++ src/DataStructures/D1/InputJollyJumpers.java
@@ -4,12 +4,13 @@ import java.io.BufferedWriter;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.OutputStreamWriter;
+import java.nio.charset.StandardCharsets;
 
 public class InputJollyJumpers {
     public static void saveAsXml(String fileName) {
         BufferedWriter outfile;
         try {
-            OutputStreamWriter writer = new OutputStreamWriter(new FileOutputStream(fileName), "UTF-8");
+            OutputStreamWriter writer = new OutputStreamWriter(new FileOutputStream(fileName), StandardCharsets.UTF_8);
             outfile = new BufferedWriter(writer);
                 outfile.write("4 \n");
                 outfile.write("1 \n");
