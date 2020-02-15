b3536525b6517195d68a3296e46bc798a856b193
oguzkeremyildiz <oguzkeremyildiz@gmail.com>
2020-02-15 16:25:49 +0300

    Initial Commit

--- src/Strings/S6/InputFileFragmentation.java
+++ src/Strings/S6/InputFileFragmentation.java
@@ -5,19 +5,31 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.OutputStreamWriter;
 import java.nio.charset.StandardCharsets;
+import java.util.Random;
 
 public class InputFileFragmentation {
     public static void saveAsXml(String fileName) {
         BufferedWriter outfile;
+        int size = 2000;
         try {
+            int number;
+            String fullNumbers = "";
+            String divide;
+            Random random = new Random();
+            int randomDivide;
             OutputStreamWriter writer = new OutputStreamWriter(new FileOutputStream(fileName), StandardCharsets.UTF_8);
             outfile = new BufferedWriter(writer);
-            outfile.write("011\n");
-            outfile.write("0111\n");
-            outfile.write("01110\n");
-            outfile.write("111\n");
-            outfile.write("0111\n");
-            outfile.write("10111");
+            for (int i = 0; i < size; i++){
+                number = random.nextInt(2);
+                fullNumbers = fullNumbers + number;
+            }
+            for (int j = 0; j < 72; j++){
+                randomDivide = random.nextInt(2000);
+                divide = fullNumbers.substring(0, randomDivide);
+                outfile.write(divide + "\n");
+                divide = fullNumbers.substring(randomDivide, 2000);
+                outfile.write(divide + "\n");
+            }
             outfile.close();
         } catch (IOException ioException) {
             System.out.println("Output file can not be opened");
