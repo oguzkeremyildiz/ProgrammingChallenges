a6ba2d6120fa8fdf45c6e2d3c7d51b25203141e9
oguzkeremyildiz <oguzkeremyildiz@gmail.com>
2020-02-22 12:19:45 +0300

    Initial Commit

---
+++ src/Sorting/InputVitosFamily.java
@@ -0,0 +1,26 @@
+package Sorting;/* Created by oguzkeremyildiz on 22.02.2020 */
+
+import java.io.BufferedWriter;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.nio.charset.StandardCharsets;
+
+public class InputVitosFamily {
+    public static void saveAsXml(String fileName) {
+        BufferedWriter outfile;
+        try {
+            OutputStreamWriter writer = new OutputStreamWriter(new FileOutputStream(fileName), StandardCharsets.UTF_8);
+            outfile = new BufferedWriter(writer);
+            outfile.write("2\n");
+            outfile.write("2 2 4\n");
+            outfile.write("3 2 4 6");
+            outfile.close();
+        } catch (IOException ioException) {
+            System.out.println("Output file can not be opened");
+        }
+    }
+    public static void main(String[]args){
+        saveAsXml("Vito.txt");
+    }
+}
