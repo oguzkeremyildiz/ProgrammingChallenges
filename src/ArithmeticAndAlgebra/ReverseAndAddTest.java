65c1309cee38c63f4f799c26420910a13ba62307
oguzkeremyildiz <oguzkeremyildiz@gmail.com>
2020-03-16 12:26:21 +0300

    Initial Commit

---
+++ src/ArithmeticAndAlgebra/ReverseAndAddTest.java
@@ -0,0 +1,13 @@
+package ArithmeticAndAlgebra;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ReverseAndAddTest {
+
+    @org.junit.jupiter.api.Test
+    void testPower() {
+        int result = ReverseAndAdd.power(5, 2);
+        assertEquals(25, result);
+    }
+
+}
