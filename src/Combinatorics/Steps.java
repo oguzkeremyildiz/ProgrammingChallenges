cc4b9fd576d446fc9a1a8e1f552d757cd092fd9a
oguzkeremyildiz <oguzkeremyildiz@gmail.com>
2020-03-16 18:33:20 +0300

    Initial Commit

--- src/Combinatorics/Steps.java
+++ src/Combinatorics/Steps.java
@@ -4,51 +4,51 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Steps {
-    public static int sayiToplam(int sayi){
-        int toplam = 0;
-        if (sayi == 0){
+    public static int numberTotal(int number){
+        int total = 0;
+        if (number == 0){
             return 0;
         }else {
-            for (int i = 1; i <= sayi; i++){
-                toplam += i;
+            for (int i = 1; i <= number; i++){
+                total += i;
             }
         }
-        return toplam;
+        return total;
     }
     public static void main(String[]args){
-        Scanner girdi = new Scanner(System.in);
-        int girdiSayisi = girdi.nextInt();
-        for (int i = 1; i <= girdiSayisi; i++){
-            int sayi1 = girdi.nextInt();
-            int sayi2 = girdi.nextInt();
-            int sayi = sayi2 - sayi1;
-            int sayiYD = 0;
-            ArrayList<Integer> kisaYol = new ArrayList<>();
-            if (sayi2 > sayi1){
-                while (sayi != 0){
-                    while (sayiToplam(sayiYD) < sayi){
-                        sayiYD++;
-                        if (sayiToplam(sayiYD) < sayi){
-                            if (sayiYD != 0){
-                                sayi -= sayiYD;
-                                kisaYol.add(sayiYD);
+        Scanner input = new Scanner(System.in);
+        int numberOfEntries = input.nextInt();
+        for (int i = 1; i <= numberOfEntries; i++){
+            int number1 = input.nextInt();
+            int number2 = input.nextInt();
+            int number = number2 - number1;
+            int numberYD = 0;
+            ArrayList<Integer> shortCut = new ArrayList<>();
+            if (number2 > number1){
+                while (number != 0){
+                    while (numberTotal(numberYD) < number){
+                        numberYD++;
+                        if (numberTotal(numberYD) < number){
+                            if (numberYD != 0){
+                                number -= numberYD;
+                                shortCut.add(numberYD);
                             }
                         }
-                    }while (sayiToplam(sayiYD) == sayi){
-                        sayi -= sayiYD;
-                        if (sayiYD != 0){
-                            kisaYol.add(sayiYD);
+                    }while (numberTotal(numberYD) == number){
+                        number -= numberYD;
+                        if (numberYD != 0){
+                            shortCut.add(numberYD);
                         }
-                    }while (sayiToplam(sayiYD) > sayi){
-                        sayiYD--;
-                        if (sayiYD != 0){
-                            sayi -= sayiYD;
-                            kisaYol.add(sayiYD);
+                    }while (numberTotal(numberYD) > number){
+                        numberYD--;
+                        if (numberYD != 0){
+                            number -= numberYD;
+                            shortCut.add(numberYD);
                         }
                     }
                 }
             }
-            System.out.print(kisaYol.size());
+            System.out.print(shortCut.size());
         }
     }
 }
