c1b27aa7be6dcac161ebe81fabf8217bf8e2c403
oguzkeremyildiz <oguzkeremyildiz@gmail.com>
2020-03-19 21:36:05 +0300

    Initial Commit

--- src/GettingStarted/G3/TheTrip.java
+++ src/GettingStarted/G3/TheTrip.java
@@ -4,45 +4,45 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class TheTrip {
-    public static void paraTransfer(ArrayList<Integer> paralar){
+    public static void moneyTransfer(ArrayList<Integer> moneys){
         int transfer = 0;
-        int toplam = 0;
+        int total = 0;
 
-        for (Integer integer : paralar) {
-            toplam += integer;
+        for (Integer integer : moneys) {
+            total += integer;
         }
-        toplam = toplam / paralar.size();
-        for (int z = 0; z < paralar.size(); z++){
-            if (paralar.get(z) < toplam){
-                transfer += toplam - paralar.get(z);
+        total = total / moneys.size();
+        for (int z = 0; z < moneys.size(); z++){
+            if (moneys.get(z) < total){
+                transfer += total - moneys.get(z);
             }
-            if (paralar.get(z) > toplam){
-                paralar.set(z, toplam);
+            if (moneys.get(z) > total){
+                moneys.set(z, total);
             }
         }
         System.out.println("$" + transfer / 100.00);
-        paralar.clear();
+        moneys.clear();
     }
     public static void main(String[]args){
-        Scanner girdi = new Scanner(System.in);
-        ArrayList<Integer> paralar = new ArrayList<>();
-        System.out.println("Kişi sayısını giriniz.");
-        int sayi = girdi.nextInt();
-        System.out.println("Paraları giriniz.");
-        for (int i = 0; i < sayi; i++){
-            paralar.add((int) (girdi.nextDouble() * 100));
+        Scanner input = new Scanner(System.in);
+        ArrayList<Integer> moneys = new ArrayList<>();
+        System.out.println("Enter the number of people:");
+        int number = input.nextInt();
+        System.out.println("Enter the moneys:");
+        for (int i = 0; i < number; i++){
+            moneys.add((int) (input.nextDouble() * 100));
         }
-        paraTransfer(paralar);
-        paralar.clear();
-        while (sayi != 0){
-            System.out.println("Kişi sayısını giriniz.");
-            sayi = girdi.nextInt();
-            if (sayi != 0){
-                System.out.println("Paraları giriniz.");
-                for (int i = 0; i < sayi; i++){
-                    paralar.add((int) (girdi.nextDouble() * 100.00));
+        moneyTransfer(moneys);
+        moneys.clear();
+        while (number != 0){
+            System.out.println("Enter the number of people:");
+            number = input.nextInt();
+            if (number != 0){
+                System.out.println("Enter the moneys:");
+                for (int i = 0; i < number; i++){
+                    moneys.add((int) (input.nextDouble() * 100.00));
                 }
-                paraTransfer(paralar);
+                moneyTransfer(moneys);
             }
         }
     }
