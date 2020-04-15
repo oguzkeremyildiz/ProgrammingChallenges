39432f177cfbe27037d3f5a3e0af8c2a5d45e8b1
oguzkeremyildiz <oguzkeremyildiz@gmail.com>
2020-04-15 03:12:57 +0300

    Initial Commit

---
+++ src/Backtracking/B7/ColorHashTest.java
@@ -0,0 +1,39 @@
+package Backtracking.B7;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.LinkedList;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ColorHashTest extends ColorHash {
+
+    @Test
+    void testRemaining() {
+        LinkedList<Integer> sequence = new LinkedList<>();
+        sequence.add(2);
+        sequence.add(2);
+        sequence.add(2);
+        sequence.add(3);
+        sequence.add(2);
+        sequence.add(4);
+        sequence.add(1);
+        sequence.add(3);
+        sequence.add(3);
+        sequence.add(3);
+        sequence.add(3);
+        sequence.add(3);
+        sequence.add(1);
+        sequence.add(1);
+        sequence.add(1);
+        sequence.add(2);
+        sequence.add(2);
+        sequence.add(6);
+        sequence.add(6);
+        sequence.add(6);
+        sequence.add(6);
+        sequence.add(6);
+        int result = ColorHash.remaining(sequence);
+        assertEquals(6, result);
+    }
+}
