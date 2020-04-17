b4c756d6ab5e5681e825caa49ce09af1ae61fbd1
oguzkeremyildiz <oguzkeremyildiz@gmail.com>
2020-04-18 01:04:58 +0300

    Initial Commit

---
+++ src/Tuples/Unit.java
@@ -0,0 +1,19 @@
+package Tuples;/* Created by oguzkeremyildiz on 18.04.2020 */
+
+public class Unit<A> {
+    A a;
+    public Unit(A a) {
+        this.a = a;
+    }
+    public A getA() {
+        return a;
+    }
+    @Override
+    public boolean equals(Object o) {
+        if (!(o instanceof Pair)) {
+            return false;
+        }
+        Unit<A> unit = (Unit<A>) o;
+        return this.a.equals(unit.a);
+    }
+}
