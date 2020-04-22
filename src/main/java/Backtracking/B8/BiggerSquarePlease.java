795821b33d70e81f78394512af8c02cac0847611
oguzkeremyildiz <oguzkeremyildiz@gmail.com>
2020-04-22 18:28:52 +0300

    Initial Commit

--- src/main/java/Backtracking/B8/BiggerSquarePlease.java
+++ src/main/java/Backtracking/B8/BiggerSquarePlease.java
@@ -1,7 +1,10 @@
 package Backtracking.B8;/* Created by oguzkeremyildiz on 17.04.2020 */
 
+import Tuples.Pair;
+
 import java.io.File;
 import java.util.Collections;
+import java.util.Date;
 import java.util.LinkedList;
 import java.util.Scanner;
 
@@ -36,7 +39,47 @@ public class BiggerSquarePlease {
         }
         return length;
     }
+    private static Pair<Integer, Integer> lengthForPossibilities(int[][] board, int i) {
+        int length = 0;
+        int current = -2;
+        for (int k = 0; k < board[0].length; k++) {
+            if (board[i][k] == 0) {
+                length++;
+                if (k + 1 >= board[0].length) {
+                    current = board[0].length - 1;
+                }
+            } else {
+                current = k - 1;
+                length = 0;
+            }
+        }
+        return new Pair<>(length, current);
+    }
+    protected static int possibleSquareSize(int[][] board) {
+        LinkedList<Pair<Integer, Integer>> lengthList = new LinkedList<>();
+        int square = 0;
+        for (int i = 0; i < board.length; i++) {
+            lengthList.add(lengthForPossibilities(board, i));
+        }
+        for (int j = 0; j < lengthList.size(); j++) {
+            if (lengthList.get(j).getKey() > 0) {
+                if (j > 1) {
+                    if (!lengthList.get(j).equals(lengthList.get(j - 1))) {
+                        square++;
+                    }
+                } else {
+                    square++;
+                }
+            }
+        }
+        return square;
+    }
     private static void constructCandidates(int[][] board, LinkedList<Integer> subsets, LinkedList<Integer> sequence) {
+        if (sequence.size() + possibleSquareSize(board) >= totalSquare) {
+            if (totalSquare > 0) {
+                return;
+            }
+        }
         boolean check = false;
         int length;
         if (sequence.size() > 0) {
@@ -159,6 +202,7 @@ public class BiggerSquarePlease {
             int size;
             for (int i = 0; i < times; i++) {
                 totalSquare = 10000;
+                Date currentDate = new Date();
                 size = source.nextInt();
                 int[][] board = new int[size][size];
                 bestBoard = new int[size][size];
@@ -168,6 +212,8 @@ public class BiggerSquarePlease {
                     totalSquare = 4;
                     fillForTwo(size);
                 }
+                Date finishDate = new Date();
+                System.out.println(finishDate.getTime() - currentDate.getTime());
                 System.out.println(totalSquare);
                 print();
             }
