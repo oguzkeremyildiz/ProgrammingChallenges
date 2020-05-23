0c40dd843f97e067b721176f4a35c3c7d7a13673
oguzkeremyildiz <oguzkeremyildiz@gmail.com>
2020-05-23 16:49:09 +0300

    Initial Commit

--- src/main/java/GraphAlgorithms/G4/Railroads.java
+++ src/main/java/GraphAlgorithms/G4/Railroads.java
@@ -5,53 +5,49 @@ import Cookies.Graph.WeightedGraph;
 import Cookies.Tuple.Pair;
 
 import java.io.File;
-import java.util.HashMap;
-import java.util.LinkedList;
-import java.util.Scanner;
-import java.util.Set;
+import java.util.*;
 
 public class Railroads {
-    private static Roads setRoads(Set<Road> roads) {
-        Roads returning = new Roads();
-        for (Road key : roads) {
-            returning.add(key);
+    private static Set<Road> addSet(Set<Road> keySet) {
+        Set<Road> current = new HashSet<>();
+        for (Road key : keySet) {
+            current.add(key);
         }
-        return returning;
+        return current;
     }
-    private static void addList(LinkedList<WeightedGraph<Road, Integer>> graphs, Roads list) {
-        if (graphs.size() > 0) {
-            for (WeightedGraph<Road, Integer> graph : graphs) {
-                Roads keySet = setRoads(graph.getKeySet());
-                Roads vertexSet = setRoads(graph.getVertexList());
-                if (!keySet.containsRoadName(list.getFirst().getName()) && vertexSet.containsRoadName(list.getFirst().getName())) {
-                    if (vertexSet.getRoad(list.getFirst().getName()).getHour() < list.getFirst().getHour()) {
-                        for (int i = 1; i < list.size(); i++) {
-                            graph.addDirectedEdge(list.getFirst(), list.get(i), list.get(i).getHour() - list.getFirst().getHour());
+    private static void addEdge(WeightedGraph<Road, Integer> graph, Roads list) {
+        if (graph.size() > 0) {
+            graph.put(list.getFirst(), new LinkedList<>());
+            for (int i = 1; i < list.size(); i++) {
+                graph.addDirectedEdge(list.getFirst(), list.get(i), Math.abs(list.get(i).getHour() - list.getFirst().getHour()));
+            }
+            for (int i = 0; i < list.size(); i++) {
+                Set<Road> keys = addSet(graph.getVertexList());
+                for (Road key : keys) {
+                    if (key.getName().equals(list.get(i).getName()) && !key.equals(list.get(i))) {
+                        if (key.getHour() < list.get(i).getHour()) {
+                            graph.addDirectedEdge(key, list.get(i), Math.abs(list.get(i).getHour() - key.getHour()));
+                        } else {
+                            graph.addDirectedEdge(list.get(i), key, Math.abs(list.get(i).getHour() - key.getHour()));
                         }
                     }
                 }
             }
-        }
-        graphs.add(new WeightedGraph<>(new IntegerLength()));
-        graphs.getLast().put(list.getFirst(), new LinkedList<>());
-        for (int i = 1; i < list.size(); i++) {
-            graphs.getLast().addDirectedEdge(list.getFirst(), list.get(i), list.get(i).getHour() - list.getFirst().getHour());
+        } else {
+            graph.put(list.getFirst(), new LinkedList<>());
+            for (int i = 1; i < list.size(); i++) {
+                graph.addDirectedEdge(list.getFirst(), list.get(i), Math.abs(list.get(i).getHour() - list.getFirst().getHour()));
+            }
         }
     }
-    private static void findPath(LinkedList<WeightedGraph<Road, Integer>> graphs, int open, String start, String finish) {
+    private static void findPath(WeightedGraph<Road, Integer> graph, int open, String start, String finish) {
         LinkedList<Pair<Road, Road>> list = new LinkedList<>();
-        for (int i = 0; i < graphs.size(); i++) {
-            if (contains(graphs.get(i), start, open)) {
-                for (Road key : graphs.get(i).getKeySet()) {
-                    if (key.getName().equals(start) && key.getHour() > open) {
-                        HashMap<Road, Pair<Integer, Road>> map = graphs.get(i).bellmanFord(key);
-                        for (Road element : map.keySet()) {
-                            if (element.getName().equals(finish)) {
-                                list.add(new Pair<>(key, element));
-                                break;
-                            }
-                        }
-                        break;
+        for (Road key : graph.getKeySet()) {
+            if (key.getName().equals(start) && key.getHour() > open) {
+                HashMap<Road, Pair<Integer, Road>> map = graph.bellmanFord(key);
+                for (Road element : map.keySet()) {
+                    if (element.getName().equals(finish) && map.get(element).getValue() != null) {
+                        list.add(new Pair<>(key, element));
                     }
                 }
             }
@@ -59,37 +55,31 @@ public class Railroads {
         if (list.size() > 0) {
             int best = Integer.MAX_VALUE;
             Pair<Road, Road> pair = null;
-            for (int i = 0; i < list.size(); i++) {
-                if (list.get(i).getValue().getHour() < best) {
-                    pair = new Pair<>(list.get(i).getKey(), list.get(i).getValue());
-                    best = list.get(i).getValue().getHour();
-                } else if (list.get(i).getValue().getHour() == best) {
+            for (Pair<Road, Road> roadPair : list) {
+                if (roadPair.getValue().getHour() < best) {
+                    pair = new Pair<>(roadPair.getKey(), roadPair.getValue());
+                    best = roadPair.getValue().getHour();
+                } else if (roadPair.getValue().getHour() == best) {
                     if (pair != null) {
-                        if (list.get(i).getKey().getHour() > pair.getKey().getHour()) {
-                            pair = new Pair<>(list.get(i).getKey(), list.get(i).getValue());
+                        if (roadPair.getKey().getHour() > pair.getKey().getHour()) {
+                            pair = new Pair<>(roadPair.getKey(), roadPair.getValue());
                         }
                     }
                 }
             }
-            System.out.println("Departure " + pair.getKey().getHour() + " " + pair.getKey().getName());
-            System.out.println("Departure " + pair.getValue().getHour() + " " + pair.getValue().getName());
+            if (pair != null) {
+                System.out.println("Departure " + pair.getKey().getHour() + " " + pair.getKey().getName());
+                System.out.println("Arrival " + pair.getValue().getHour() + " " + pair.getValue().getName());
+            }
         } else {
             System.out.println("No connection");
         }
-    }
-    private static boolean contains(WeightedGraph<Road, Integer> graph, String target, int open) {
-        for (Road key : graph.getKeySet()) {
-            if (key.getName().equals(target) && key.getHour() > open) {
-                return true;
-            }
-        }
-        return false;
     }
     public static void main(String[]args) {
         try {
             Scanner source = new Scanner(new File("Road.txt"));
             Roads list = new Roads();
-            LinkedList<WeightedGraph<Road, Integer>> graphs = new LinkedList<>();
+            WeightedGraph<Road, Integer> graph = new WeightedGraph<>(new IntegerLength());
             String times = source.next();
             for (int i = 0; i < Integer.parseInt(times); i++) {
                 String stations = source.next();
@@ -100,15 +90,15 @@ public class Railroads {
                         String name = source.next();
                         list.add(new Road(name, Integer.parseInt(hour)));
                     }
-                    addList(graphs, list);
+                    addEdge(graph, list);
                     list.clear();
                 }
                 String open = source.next();
                 String start = source.next();
                 String finish = source.next();
                 System.out.println("Scenario " + (i + 1));
-                findPath(graphs, Integer.parseInt(open), start, finish);
-                graphs.clear();
+                findPath(graph, Integer.parseInt(open), start, finish);
+                graph.clear();
                 list.clear();
             }
         } catch (Exception e) {
--- src/main/java/GraphAlgorithms/G4/Road.java
+++ src/main/java/GraphAlgorithms/G4/Road.java
@@ -1,5 +1,7 @@
 package GraphAlgorithms.G4;/* Created by oguzkeremyildiz on 22.05.2020 */
 
+import Cookies.Tuple.Pair;
+
 public class Road {
 
     private String name;
@@ -24,5 +26,14 @@ public class Road {
 
     public void setHour(int hour) {
         this.hour = hour;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (!(o instanceof Road)) {
+            return false;
+        }
+        Road road = (Road) o;
+        return this.getName().equals(road.getName()) && this.getHour() == road.getHour();
     }
 }
