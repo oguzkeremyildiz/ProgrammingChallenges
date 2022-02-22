17097cfd33885b6d585513309b7d9aa354aa8c6a
oguzkeremyildiz <oguzkeremyildiz@gmail.com>
2022-02-22 22:23:39 +0300

    Initial Commit

--- src/main/java/DataStructures/D8/Yahtzee2.java
+++ src/main/java/DataStructures/D8/Yahtzee2.java
@@ -20,51 +20,37 @@ public class Yahtzee2 {
     }
 
     private static int[] solveDp(int index, HashMap<Triplet<String, Integer, Integer>, int[]> map, int[][] scores, String current, int currentScore) {
+        if (index == 13) {
+            return new int[15];
+        }
         int onesCount = onesCount(current);
         if (map.containsKey(new Triplet<>(current, index, onesCount))) {
             return map.get(new Triplet<>(current, index, onesCount));
         }
-        int bestIndex = -1;
-        int bestPoint = 0;
-        int[] currentPoints = new int[15];
+        int[] bestPoints = new int[15];
         for (int i = 0; i < 13; i++) {
             if (scores[index][i] > 0 && current.charAt(i) == '0') {
                 String str = current.substring(0, i) + "1" + current.substring(i + 1);
-                if (index < 12) {
-                    int plus = scores[index][i];
-                    if (index < 6 && currentScore < 63 && currentScore + scores[index][i] > 62) {
-                        plus += 35;
+                int plus = scores[index][i];
+                if (index < 6 && currentScore < 63 && currentScore + scores[index][i] > 62) {
+                    plus += 35;
+                }
+                int[] points = solveDp(index + 1, map, scores, str, currentScore + plus);
+                map.put(new Triplet<>(str, index, onesCount(current)), points);
+                if (points[14] + plus > bestPoints[14]) {
+                    bestPoints = points.clone();
+                    bestPoints[14] = points[14] + plus;
+                    if (plus - scores[index][i] > 0) {
+                        bestPoints[13] = 35;
                     }
-                    int[] points = solveDp(index + 1, map, scores, str, currentScore + plus);
-                    map.put(new Triplet<>(str, index, onesCount(current)), points);
-                    if (points[14] + plus > bestPoint) {
-                        bestIndex = i;
-                        bestPoint = points[14] + plus;
-                        currentPoints = points.clone();
-                    }
-                } else {
-                    if (scores[index][i] > bestPoint) {
-                        bestIndex = i;
-                        bestPoint = scores[index][i];
-                    }
+                    bestPoints[index] = scores[index][i];
                 }
             }
         }
-        if (index < 12) {
-            int[] points = solveDp(index + 1, map, scores, current, currentScore);
-            map.put(new Triplet<>(current, index + 1, onesCount(current)), points);
-            if (points[14] > bestPoint) {
-                return points;
-            }
-        }
-        if (bestIndex == -1) {
-            return currentPoints;
-        }
-        int[] bestPoints = currentPoints.clone();
-        bestPoints[index] = scores[index][bestIndex];
-        bestPoints[14] = bestPoint;
-        if (index < 6 && currentScore < 63 && currentScore + scores[index][bestIndex] > 62) {
-            bestPoints[13] = 35;
+        int[] points = solveDp(index + 1, map, scores, current, currentScore);
+        map.put(new Triplet<>(current, index + 1, onesCount(current)), points);
+        if (points[14] > bestPoints[14]) {
+            return points;
         }
         return bestPoints;
     }
