4ac41859de1f00895ce81d925cf3453449ec0b8a
oguzkeremyildiz <oguzkeremyildiz@gmail.com>
2022-02-24 21:29:02 +0300

    Initial Commit

--- src/main/java/DataStructures/D8/Yahtzee2.java
+++ src/main/java/DataStructures/D8/Yahtzee2.java
@@ -9,21 +9,10 @@ import java.util.Scanner;
 
 public class Yahtzee2 {
 
-    private static int onesCount(String str) {
-        int count = 0;
-        for (int i = 0; i < str.length(); i++) {
-            if (str.charAt(i) == '1') {
-                count++;
-            }
-        }
-        return count;
-    }
-
-    private static int[] solveDp(int index, HashMap<Triplet<String, Integer, Integer>, int[]> map, int[][] scores, String current, int currentScore) {
+    private static int[] solveDp(int index, HashMap<Triplet<String, Integer, Integer>, int[]> map, int[][] scores, String current, int currentScore, int onesCount) {
         if (index == 13) {
             return new int[15];
         }
-        int onesCount = onesCount(current);
         if (map.containsKey(new Triplet<>(current, index, index - onesCount))) {
             return map.get(new Triplet<>(current, index, index - onesCount));
         }
@@ -35,8 +24,8 @@ public class Yahtzee2 {
                 if (index < 6 && currentScore < 63 && currentScore + scores[index][i] > 62) {
                     plus += 35;
                 }
-                int[] points = solveDp(index + 1, map, scores, str, currentScore + plus);
-                map.put(new Triplet<>(str, index, index + 1 - onesCount(str)), points);
+                int[] points = solveDp(index + 1, map, scores, str, currentScore + plus, onesCount + 1);
+                map.put(new Triplet<>(str, index, index - onesCount), points);
                 if (points[14] + plus > bestPoints[14]) {
                     bestPoints = points.clone();
                     bestPoints[14] = points[14] + plus;
@@ -47,8 +36,8 @@ public class Yahtzee2 {
                 }
             }
         }
-        int[] points = solveDp(index + 1, map, scores, current, currentScore);
-        map.put(new Triplet<>(current, index + 1, index + 1 - onesCount(current)), points);
+        int[] points = solveDp(index + 1, map, scores, current, currentScore, onesCount);
+        map.put(new Triplet<>(current, index + 1, index + 1 - onesCount), points);
         if (points[14] > bestPoints[14]) {
             return points;
         }
@@ -62,9 +51,12 @@ public class Yahtzee2 {
                 scores[j][i] = rounds.get(i).calculate(j);
             }
         }
-        int[] best = solveDp(0, new HashMap<>(), scores, "0000000000000", 0);
-        for (int j : best) {
-            System.out.print(j + " ");
+        int[] best = solveDp(0, new HashMap<>(), scores, "0000000000000", 0, 0);
+        for (int i = 0; i < best.length; i++) {
+            System.out.print(best[i]);
+            if (i + 1 != best.length) {
+                System.out.print(" ");
+            }
         }
         System.out.println();
     }
@@ -73,9 +65,7 @@ public class Yahtzee2 {
         try {
             ArrayList<Round> rounds = new ArrayList<>();
             Scanner source = new Scanner(new File("Yahtzee.txt"));
-            int co = 0;
             while (source.hasNext()) {
-                co++;
                 for (int i = 0; i < 13; i++) {
                     rounds.add(new Round(source.nextLine()));
                 }
